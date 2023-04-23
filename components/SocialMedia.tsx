20c8a65abc3e1863fe7ad6af9169d0ac52e31ab9
VedRock <hovedantgupta@gmail.com>
2023-04-23 14:13:24 +0530

    ThirdCommit

---
+++ components/SocialMedia.tsx
@@ -0,0 +1,70 @@
+import React from "react";
+
+function SocialMedia() {
+  return (
+    <div className="SocialOne">
+      <ul className="effect01">
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-youtube"></i>
+            <span>Youtube</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-twitter"></i>
+            <span>Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-whatsapp"></i>
+            <span>Whatsapp</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="effect02">
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-youtube"></i>
+            <span>Youtube</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-twitter"></i>
+            <span>Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-whatsapp"></i>
+            <span>Whatsapp</span>
+          </a>
+        </li>
+      </ul>
+      <ul className="effect03">
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-youtube"></i>
+            <span>Youtube</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-twitter"></i>
+            <span>Twitter</span>
+          </a>
+        </li>
+        <li>
+          <a href="#">
+            <i className="fa-brands fa-whatsapp"></i>
+            <span>Whatsapp</span>
+          </a>
+        </li>
+      </ul>
+    </div>
+  );
+}
+
+export default SocialMedia;
